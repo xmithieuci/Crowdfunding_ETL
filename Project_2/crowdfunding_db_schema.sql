98bf3e066dcd5d50939faacb07f006037ce071d2
unknown <hetalprajapati256@gmail.com>
2025-01-06 20:19:38 -0800

    Initial commit

--- Project_2/crowdfunding_db_schema.sql
+++ Project_2/crowdfunding_db_schema.sql
@@ -1,10 +1,17 @@
+-- Create database
+CREATE DATABASE crowdfunding_db;
+
+
 -- contacts table
 CREATE TABLE contacts (
     contact_id Int PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    email VARCHAR(50)
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL
 );
+
+-- Show all data in the contacts table
+SELECT * FROM contacts;
 
 -- categories table
 CREATE TABLE category (
@@ -12,11 +19,17 @@ CREATE TABLE category (
     category VARCHAR(30) NOT NULL
 );
 
+-- Show all data in the category table
+SELECT * FROM category;
+
 -- subcategories table
 CREATE TABLE subcategory (
     subcategory_id VARCHAR(10) PRIMARY KEY,
     subcategory VARCHAR(30) NOT NULL
 );
+
+-- Show all data in the subcategory table
+SELECT * FROM subcategory;
 
 -- campaigns table
 CREATE TABLE campaign (
@@ -35,3 +48,6 @@ CREATE TABLE campaign (
     category_id VARCHAR(10) REFERENCES category(category_id),
     subcategory_id VARCHAR(10) REFERENCES subcategory(subcategory_id)
 );
+
+-- Show all data in the campaign table
+SELECT * FROM campaign;
